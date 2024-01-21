3930abebdea32ff379d76ccb07c5b40d95fee694
AngerOverApathy <violasian@gmail.com>
2024-01-21 11:16:49 -0800

    style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [name, setName] = useState('')
+
 
   return (
     <>
+      <div className='App'>
+        <h1>Build a Hero</h1>
+        <label>Name:</label>
+        <input type='text' placeholder='Hero Name' style={{ margin: 10 }} />
+        <label>Age:</label>
+        <input type='number' placeholder='Hero Age' style={{ margin: 10 }} />
+        <label>Height:</label>
+        <input type='text' placeholder='Hero Height' style={{ margin: 10 }} />
+        <label>Superpower:</label>
+        <input type='text' placeholder='Hero Superpower' style={{ margin: 10 }} />
+      </div>
     </>
   )
 }
 
-export default App
+// export default App
